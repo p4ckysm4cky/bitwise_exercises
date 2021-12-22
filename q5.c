5699f2da87b1ea466b1a5bf68da8d36b4fbea0fd
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-12-23 09:39:40 +1300

    Added q5

---
+++ q5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void clear_nth_bit(int *num, int n)
+{
+    int bit_mask = ~(1 << n);
+    *num = *num & bit_mask;
+}
+
+
+int main(void)
+{
+    for (int i = 0; i < 16; i++) {
+        int x = i;
+        clear_nth_bit(&x, 0);
+        printf("%d\n", x);
+    }
+}
